6f4d9577249826f4a37882449760dc14570f90f2
B1oodyAnge1 <romsm@bk.ru>
2024-08-03 18:40:57 +0300

    Create start files

---
+++ The_larger_and_smaller_digits_of_the_number/The_larger_and_smaller_digits_of_the_number.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+#include <regex>
+
+
+int inputInt(std::string);
+
+int main()
+{
+    return 0;
+}
+
+int inputInt(std::string textReq = "Enter an integer number") { // textReq текстовый запрос у пользователя
+    std::string number;
+    std::regex reg("^[0-9]"); //регулярное выражение
+    while (true)
+    {
+        std::cout << textReq << std::endl;
+        std::cin >> number;
+        if (std::regex_search(number, reg)) {
+            return stoi(number); //преобразование string в int. С++ 11
+        }
+    }
+}
